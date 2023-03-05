cda0d94d37f98fcd7b2becefb605c208627727ae
1avinashMairal <106060767+1avinashMairal@users.noreply.github.com>
2023-03-05 15:54:26 +0530

    Create dashboard1_json.dart

---
+++ dashboard1_json.dart
@@ -0,0 +1,464 @@
+class DashboardJson1 {
+  static List<Map<dynamic, dynamic>> dashboardJson1 = [
+    //Resourse Title row
+    {
+      "_id": "63e20a959f83e075ee8cb6bb",
+      "page_id": "63e20a94f87f49390ae9f4c7",
+      "key": "name",
+      "value": "",
+      "widgetType": "titleRowButton",
+      "basicProperties": {
+        "padding": {
+          "left": "",
+          "right": "",
+          "top": "",
+          "bottom": "",
+        },
+        "margin": {
+          "left": "",
+          "right": "",
+          "top": "",
+          "bottom": "",
+        },
+        "height": "",
+        "width": "",
+        "backgroundColor": "",
+        "borderRadius": "10",
+        "border": {
+          "color": "", //#ac1291
+          "width": "1",
+        }
+      },
+      "widgetProperties": {
+        "title": "Resources",
+        "titleSize": "",
+        "titleColor": "",
+        "buttonText": "View All", // default is View All, See More
+        "buttonTextSize": "",
+        "hideViewAll": "", //default it is false //to hide button set true
+        "buttonTextColor": "" //#E55039 default is blue,
+      },
+    },
+
+    //Resourse grid view section
+    {
+      "_id": "63e20a959f83e075ee8cb6bb",
+      "page_id": "63e20a94f87f49390ae9f4c7",
+      "key": "name",
+      "value": "",
+      "widgetType": "gridView",
+      "basicProperties": {
+        "padding": {
+          "left": "",
+          "right": "",
+          "top": "",
+          "bottom": "",
+        },
+        "margin": {
+          "left": "",
+          "right": "",
+          "top": "20",
+          "bottom": "",
+        },
+        "height": "",
+        "width": "",
+        "backgroundColor": "",
+        "borderRadius": "",
+        "border": {
+          "color": "", //#ac1291
+          "width": "",
+        }
+      },
+      "widgetProperties": {
+        "itemWidget": {
+          "type": "imageTextColumn",
+          "itemCount": "6",
+          "itemProperties": {
+            "titleColor": "",
+            "titleSize": "",
+            "hideText": "",
+            "itemData": [
+              {
+                "title": "Live",
+                "url":
+                    "https://user-images.githubusercontent.com/106060767/222734682-72dc14b4-73fa-4049-b2a4-07a185bdbf1e.png",
+              },
+              {
+                "title": "Old",
+                "url":
+                    "https://user-images.githubusercontent.com/106060767/222734695-6976ecee-167a-4b75-9c3c-890faf71b4e0.png",
+              },
+              {
+                "title": "Old",
+                "url":
+                    "https://user-images.githubusercontent.com/106060767/222734695-6976ecee-167a-4b75-9c3c-890faf71b4e0.png",
+              },
+              {
+                "title": "Old",
+                "url":
+                    "https://user-images.githubusercontent.com/106060767/222734695-6976ecee-167a-4b75-9c3c-890faf71b4e0.png",
+              },
+              {
+                "title": "Old",
+                "url":
+                    "https://user-images.githubusercontent.com/106060767/222734695-6976ecee-167a-4b75-9c3c-890faf71b4e0.png",
+              },
+              {
+                "title": "Old",
+                "url":
+                    "https://user-images.githubusercontent.com/106060767/222734695-6976ecee-167a-4b75-9c3c-890faf71b4e0.png",
+              }
+            ]
+          },
+        }
+      },
+    },
+
+    //My Upcoming Sevas Title row
+    {
+      "_id": "63e20a959f83e075ee8cb6bb",
+      "page_id": "63e20a94f87f49390ae9f4c7",
+      "key": "name",
+      "value": "",
+      "widgetType": "titleRowButton",
+      "basicProperties": {
+        "padding": {
+          "left": "",
+          "right": "",
+          "top": "20",
+          "bottom": "",
+        },
+        "margin": {
+          "left": "",
+          "right": "",
+          "top": "",
+          "bottom": "",
+        },
+        "height": "",
+        "width": "",
+        "backgroundColor": "",
+        "borderRadius": "10",
+        "border": {
+          "color": "", //#ac1291
+          "width": "1",
+        }
+      },
+      "widgetProperties": {
+        "title": "My Upcoming Sevas",
+        "titleSize": "",
+        "titleColor": "",
+        "buttonText": "View All", // default is View All, See More
+        "buttonTextSize": "",
+        "hideViewAll": "", //default it is false //to hide button set true
+        "buttonTextColor": "" //#E55039 default is blue,
+      },
+    },
+
+    //My Upcoming Sevas slider section
+    {
+      "_id": "63e20a959f83e075ee8cb6bb",
+      "page_id": "63e20a94f87f49390ae9f4c7",
+      "key": "name",
+      "value": "",
+      "widgetType": "customSlider",
+      "basicProperties": {
+        "padding": {
+          "left": "",
+          "right": "",
+          "top": "20",
+          "bottom": "20",
+        },
+        "margin": {
+          "left": "",
+          "right": "",
+          "top": "",
+          "bottom": "",
+        },
+        "height": "",
+        "width": "",
+        "backgroundColor": "",
+        "borderRadius": "10",
+        "border": {
+          "color": "", //#ac1291
+          "width": "1",
+        }
+      },
+      "widgetProperties": {
+        "cardHeight": "100",
+        "cardWidth": "250",
+        "autoPlay": "true",
+        "itemCount": "2",
+        "itemProperties": {
+          "color": "", //#ac1291
+          "titleColor": "",
+          "titleSize": "",
+          "subTitleSize": "",
+          "subTitleColor": "",
+          "height": "100",
+          "shadowColor": "",
+          "borderRadius": "10",
+          "itemData": [
+            {
+              "title": "Silder card 1",
+              "subTitle": "This is jsut for demostration",
+              "imgUrl":
+                  "https://user-images.githubusercontent.com/106060767/222727277-10aba637-4f6e-4408-80c3-7e7e31db9d26.png",
+            },
+            {
+              "title": "Silder card 2",
+              "subTitle": "This is jsut for demostration",
+              "imgUrl":
+                  "https://user-images.githubusercontent.com/106060767/222727277-10aba637-4f6e-4408-80c3-7e7e31db9d26.png",
+            }
+          ]
+        },
+      },
+    },
+
+    //This Month Title row
+    {
+      "_id": "63e20a959f83e075ee8cb6bb",
+      "page_id": "63e20a94f87f49390ae9f4c7",
+      "key": "name",
+      "value": "",
+      "widgetType": "titleRowButton",
+      "basicProperties": {
+        "padding": {
+          "left": "",
+          "right": "",
+          "top": "20",
+          "bottom": "",
+        },
+        "margin": {
+          "left": "",
+          "right": "",
+          "top": "",
+          "bottom": "",
+        },
+        "height": "",
+        "width": "",
+        "backgroundColor": "",
+        "borderRadius": "10",
+        "border": {
+          "color": "", //#ac1291
+          "width": "1",
+        }
+      },
+      "widgetProperties": {
+        "title": "This Month",
+        "titleSize": "",
+        "titleColor": "",
+        "buttonText": "View All", // default is View All, See More
+        "buttonTextSize": "",
+        "hideViewAll": "", //default it is false //to hide button set true
+        "buttonTextColor": "" //#E55039 default is blue,
+      },
+    },
+
+    //This month listview section
+    {
+      "_id": "63e20a959f83e075ee8cb6bb",
+      "page_id": "63e20a94f87f49390ae9f4c7",
+      "key": "name",
+      "value": "",
+      "widgetType": "listView",
+      "basicProperties": {
+        "padding": {
+          "left": "15",
+          "right": "15",
+          "top": "15",
+          "bottom": "15",
+        },
+        "margin": {
+          "left": "",
+          "right": "",
+          "top": "20",
+          "bottom": "",
+        },
+        "height": "150",
+        "width": "",
+        "backgroundColor": "",
+        "borderRadius": "",
+        "border": {
+          "color": "", //#ac1291
+          "width": "",
+        }
+      },
+      "widgetProperties": {
+        "itemSpacing": "10",
+        "scrollDirection": "vertical",
+        "itemWidget": {
+          "type": "listTileDateCard",
+          "itemCount": "6",
+          "itemProperties": {
+            "height": "80",
+            "width": "330",
+            "titleColor": "",
+            "titleSize": "",
+            "cardColor": "", //#ac1291
+            "leadingTextSize": "",
+            "leadingTextColor": "",
+            "leadingBoxColor": "",
+            "borderRadius": "8",
+            "leadingBoxBorderRadius": "4",
+            "itemData": [
+              {
+                "title": "Silder card example1",
+                "date": "22",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example2",
+                "date": "26",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example3",
+                "date": "27",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example4",
+                "date": "28",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example5",
+                "date": "29",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example6",
+                "date": "30",
+                "month": "Jan",
+              }
+            ]
+          },
+        }
+      },
+    },
+
+    //Jagadguru Speaks Title row
+    {
+      "_id": "63e20a959f83e075ee8cb6bb",
+      "page_id": "63e20a94f87f49390ae9f4c7",
+      "key": "name",
+      "value": "",
+      "widgetType": "titleRowButton",
+      "basicProperties": {
+        "padding": {
+          "left": "",
+          "right": "",
+          "top": "20",
+          "bottom": "",
+        },
+        "margin": {
+          "left": "",
+          "right": "",
+          "top": "",
+          "bottom": "",
+        },
+        "height": "",
+        "width": "",
+        "backgroundColor": "",
+        "borderRadius": "10",
+        "border": {
+          "color": "", //#ac1291
+          "width": "1",
+        }
+      },
+      "widgetProperties": {
+        "title": "Jagadguru Speaks",
+        "titleSize": "",
+        "titleColor": "",
+        "buttonText": "View All", // default is View All, See More
+        "buttonTextSize": "",
+        "hideViewAll": "", //default it is false //to hide button set true
+        "buttonTextColor": "" //#E55039 default is blue,
+      },
+    },
+
+    //Jagadguru Speaks listview section
+    {
+      "_id": "63e20a959f83e075ee8cb6bb",
+      "page_id": "63e20a94f87f49390ae9f4c7",
+      "key": "name",
+      "value": "",
+      "widgetType": "listView",
+      "basicProperties": {
+        "padding": {
+          "left": "15",
+          "right": "15",
+          "top": "15",
+          "bottom": "15",
+        },
+        "margin": {
+          "left": "",
+          "right": "",
+          "top": "",
+          "bottom": "",
+        },
+        "height": "100",
+        "width": "",
+        "backgroundColor": "",
+        "borderRadius": "",
+        "border": {
+          "color": "", //#ac1291
+          "width": "",
+        }
+      },
+      "widgetProperties": {
+        "itemSpacing": "10",
+        "scrollDirection": "vertical",
+        "itemWidget": {
+          "type": "listTileDateCard",
+          "itemCount": "6",
+          "itemProperties": {
+            "height": "80",
+            "width": "330",
+            "titleColor": "",
+            "titleSize": "",
+            "cardColor": "", //#ac1291
+            "leadingTextSize": "",
+            "leadingTextColor": "",
+            "leadingBoxColor": "",
+            "borderRadius": "8",
+            "leadingBoxBorderRadius": "4",
+            "itemData": [
+              {
+                "title": "Silder card example1",
+                "date": "22",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example2",
+                "date": "26",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example3",
+                "date": "27",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example4",
+                "date": "28",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example5",
+                "date": "29",
+                "month": "Jan",
+              },
+              {
+                "title": "Silder card example6",
+                "date": "30",
+                "month": "Jan",
+              }
+            ]
+          },
+        }
+      },
+    }
+  ];
+}
